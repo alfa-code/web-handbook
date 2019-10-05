8f1eb13b8b4c5a4d0b63378e596e98885cefa6cd
Вечканов Алексей <avechkanov@alfabank.ru>
2019-10-05 20:44:35 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    extends: [
+        require.resolve('arui-presets-lint/eslint')
+    ],
+    rules: {
+        'sort-class-members/sort-class-members': 'off',
+        'arrow-body-style': 'off',
+        'quote-props': 'off',
+        'max-len': 'warn',
+        'no-undef': ['error', { 'typeof': true }],
+        'no-restricted-syntax': ['error', 'WithStatement', 'LabeledStatement'],
+        'no-continue': 'off',
+        'operator-linebreak': 'off',
+        'no-restricted-globals': 'off',
+        'import/no-unresolved': 'off',
+        'react/no-deprecated': 'error',
+        'react/no-danger': 'warn',
+        'react/jsx-no-undef': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'react/no-unsafe': ['warn', { "checkAliases": true }],
+        'jsx-control-statements/jsx-use-if-tag': 'off',
+        'react/no-did-update-set-state': 'off',
+        'react/no-did-mount-set-state': 'off',
+        'import/extensions': ['error', {
+            'tsx': 'never',
+            'pdf': 'always',
+            'svg': 'always',
+            'json': 'always',
+            'css': 'always'
+        }],
+        'import/order': ['error', {
+            'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+            'newlines-between': 'always-and-inside-groups'
+        }]
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                moduleDirectory: ['node_modules', 'packages']
+            }
+        }
+    },
+    env: {
+        jest: true
+    }
+};
