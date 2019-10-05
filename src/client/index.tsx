126ea857328c51add7745b217ec64f75c5ade200
Вечканов Алексей <avechkanov@alfabank.ru>
2019-10-05 20:26:57 +0300

    added the normalize.css

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app/app';
+
+import 'normalize.css';
   
 ReactDOM.render(<App />, document.getElementById('react-app'));
