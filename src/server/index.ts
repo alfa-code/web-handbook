b09424297f3e3291fb0026f3c3c4acb1762fb0c0
Hydrock <hydrock74@gmail.com>
2020-01-07 13:21:55 +0300

    add tls for https (self signed)

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import { getContent } from './content';
 
+import * as fs from 'fs';
+
 const path = require('path');
 
 const Hapi = require('@hapi/hapi');
@@ -7,28 +9,49 @@ const Inert = require('@hapi/inert');
 
 const rootPath = process.cwd();
 
-const PORT = 80;
+const HTTPS_PORT = 443;
+const HTTP_PORT = 80;
+const HOST = '0.0.0.0';
 
-const server = new Hapi.Server({
-    port: PORT,
-    host: '0.0.0.0',
+const httpServer = new Hapi.Server({
+    port: HTTP_PORT,
+    host: HOST,
+});
+
+const httpsServer = new Hapi.Server({
+    port: HTTPS_PORT,
+    host: HOST,
     routes: {
         files: {
             relativeTo: path.join(rootPath, '.build/', 'assets/'),
         },
     },
+    tls: {
+        /* tslint:disable-next-line */
+        key: fs.readFileSync(path.join(rootPath,  'tls/', 'server.key')),
+        /* tslint:disable-next-line */
+        cert: fs.readFileSync(path.join(rootPath,  'tls/', 'server.cert'))
+    }
 });
 
 const init = async () => {
-    await server.register(Inert);
+    await httpsServer.register(Inert);
 
-    //   server.route({
+    httpServer.route({
+        method: '*',
+        path: '/{any*}',
+        handler: function (request, reply) {
+            return reply.redirect(`https://${request.url.host}${request.url.pathname}${request.url.search}`);
+        }
+    });
+
+    //   httpsServer.route({
     //     method: '*',
     //     path: '/{any*}',
     //     handler: () => '404 Error! Page Not Found!',
     //   });
 
-    server.route({
+    httpsServer.route({
         method: '*',
         path: '/{any*}',
         handler: (request) => {
@@ -36,7 +59,7 @@ const init = async () => {
         }
     });
 
-    server.route({
+    httpsServer.route({
         method: 'GET',
         path: '/',
         handler: (request) => {
@@ -52,7 +75,7 @@ const init = async () => {
     //     },
     // });
 
-    server.route({
+    httpsServer.route({
         method: 'GET',
         path: '/assets/{param*}',
         handler: {
@@ -64,9 +87,11 @@ const init = async () => {
         },
     });
 
-    await server.start();
+    await httpsServer.start();
+    await httpServer.start();
     // eslint-disable-next-line no-console
-    console.log('Server running on %s', server.info.uri, `development (http://localhost:${PORT})`);
+    console.log('HTTPS Server running on %s', httpsServer.info.uri, `development (http://localhost:${HTTPS_PORT})`);
+    console.log('HTTP Server running on %s', httpServer.info.uri, `development (http://localhost:${HTTP_PORT})`);
 };
 
 process.on('unhandledRejection', (err) => {
