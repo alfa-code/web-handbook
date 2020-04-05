16705400695d649081da2976efa7c389f667a6d2
Hydrock <hydrock74@gmail.com>
2020-04-05 05:30:54 +0300

    fix the menu - create links

--- src/client/components/header-navigation/header-navigation.tsx
+++ src/client/components/header-navigation/header-navigation.tsx
@@ -1,29 +1,38 @@
 import React, { PureComponent, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './header-navigation.module.scss';
 
-interface Props {}
-interface State {}
+interface Props { }
+interface State { }
 
 export class HeaderNavigation extends PureComponent<Props, State> {
-  render(): ReactNode {
-    return (
-      <nav className={styles.headerNavigation}>
-        <ul>
-          <li>
-                        Курсы
-          </li>
-          <li>
-                        Блог
-          </li>
-          <li>
-                        Html справочник
-          </li>
-          <li>
-                        О нас
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+    render(): ReactNode {
+        return (
+            <nav className={styles.headerNavigation}>
+                <ul>
+                    <li>
+                        <Link to='/'>
+                            Курсы
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/'>
+                            Блог
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/'>
+                            Html справочник
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/'>
+                            О нас
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
 }
