519f68bdc685a5b4a018738431c4376a57f5dbd0
startsev.alexey <aistartsev@alfabank.ru>
2020-05-22 15:42:05 +0300

    ACP-5: added style wrappers fabric

--- src/client/blocks/user-data-form-block/user-data-form-block.tsx
+++ src/client/blocks/user-data-form-block/user-data-form-block.tsx
@@ -1,12 +1,8 @@
 import React, { ReactNode } from 'react';
 import { Heading } from 'Components/heading';
 import { InputSimple } from 'Components/input-simple';
-import { ProfileForm } from 'Components/style-wrappers/profile-form';
-
-import styles from './user-data-form-block.module.scss';
+import { ProfileForm, FormBody, SubmitBlock } from 'Components/style-wrappers/profile';
 import { Button } from 'Components/button';
-import { FormBody } from 'Components/style-wrappers/form-body';
-
 
 export class UserDataFormBlock extends React.PureComponent {
     render(): ReactNode {
@@ -18,10 +14,10 @@ export class UserDataFormBlock extends React.PureComponent {
                         <InputSimple className='form-body-input' name='name' placeholder='Имя' size='full'/>
                         <InputSimple className='form-body-input' name='surname' placeholder='Фамилия' size='full'/>
                     </FormBody>
-                    <div className={styles.submitBlock}>
-                        <Button className={styles.button} viewType='primary'>Сохранить</Button>
-                        <Button className={styles.button} viewType='secondary'>Удалить аккаунт</Button>
-                    </div>
+                    <SubmitBlock>
+                        <Button viewType='primary'>Сохранить</Button>
+                        <Button viewType='secondary'>Удалить аккаунт</Button>
+                    </SubmitBlock>
                 </ProfileForm>
             </React.Fragment>
         );
