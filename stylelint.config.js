7e4f77aa8c93796ea40ff76c8ed986adba51c18d
Hydrock <hydrock@yandex.ru>
2020-11-15 15:43:55 +0300

    fixes

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    extends: '@alfa-code/alfa-code-presets/stylelint/index.js',
-    rules: {
-        'font-family-name-quotes': 'always-unless-keyword',
-        'selector-max-type': 1,
-        'primer/no-override': false
-    }
+    extends: '@alfa-code/alfa-code-presets/stylelint/index.js'
 };
