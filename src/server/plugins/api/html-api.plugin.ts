59dd025964540d32ea841a3854f5f7dc081bee0d
Hydrock <hydrock@yandex.ru>
2020-11-22 12:03:05 +0300

    feat(api): worked on api

--- src/server/plugins/api/html-api.plugin.ts
+++ src/server/plugins/api/html-api.plugin.ts
@@ -1,11 +1,12 @@
 import { ResponseToolkit } from '@hapi/hapi';
+import axios from 'axios';
 
 import { getHtmlTagsListByAlphabet, isHtmlTagExists } from 'Utils/html';
 
 export const htmlApiPlugin = {
     name: 'htmlApiPlugin',
     version: '1.0.0',
-    register: async function (server, options) {
+    register: async function (server, _options) {
         let htmlTags;
 
         server.route({
@@ -23,23 +24,44 @@ export const htmlApiPlugin = {
         server.route({
             method: 'GET',
             path: '/api/html/{tag}',
-            handler: function (request, h: ResponseToolkit) {
+            handler: async function (request, h: ResponseToolkit) {
                 const { params: { tag } } = request;
-
 
                 const isTagExists = isHtmlTagExists(tag);
                 console.log('isTagExists', isTagExists)
 
                 if (!isTagExists) {
                     return h.response({
-                        message: 'tag not found'
+                        message: 'tag does not exists'
                     }).code(404);
                 }
 
-                return h.response({
-                    message: 'found',
-                    data: { tag: tag }
-                }).code(200);
+                try {
+                    const tagInfo = await axios.get(`https://raw.githubusercontent.com/alfa-code/web-handbook-materials/main/materials/html/tags/${tag}/main.json`);
+                    const { status, data } = tagInfo;
+
+                    return h.response({
+                        message: 'found',
+                        data
+                    }).code(status);
+
+                } catch (error) {
+                    if (error.response) {
+                        const { status, data } = error.response;
+
+                        return h.response({
+                            message: 'tag is not found',
+                            githubStatus: status,
+                            githubMessage: data
+                        }).code(404);
+                    }
+
+                    console.log(error);
+
+                    return h.response({
+                        message: 'server error',
+                    }).code(500);
+                }
             }
         });
     }
